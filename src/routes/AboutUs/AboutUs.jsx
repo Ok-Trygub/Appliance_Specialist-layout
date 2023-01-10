69a39e8980f6d62f333820e277d215a3b4b61d6f
unknown <ok.trygub@gmail.com>
2023-01-10 09:42:05 +0200

    add about page

--- src/routes/AboutUs/AboutUs.jsx
+++ src/routes/AboutUs/AboutUs.jsx
@@ -3,6 +3,7 @@ import AboutOffer from '../../components/AboutOffer/AboutOffer';
 import AboutIcon from "../../assets/icons/About";
 import AboutMainImg from '../../assets/images/about_mainImg.png';
 import './AboutUs.css';
+import MasterImg from '../../assets/images/master.png';
 
 const AboutUs = () => {
     const offerText = 'Lorem ipsum dolor sit amet consectetur. Tincidunt vitae vel ut mauris libero quis morbi. Ut habitasse nisi mollis sapien.'
@@ -16,25 +17,33 @@ const AboutUs = () => {
                 <AboutIcon/>
             </AboutOffer>
 
-            <section>
-                <h2 className='mb-12 containerStyle'><span className='headerUnderline'>Our</span> way
+            <section className='containerStyle lg:px-30 xl:px-36 pb-9 sm:pb-11 lg:pb-20 xl:pb-[120px]'>
+                <h2 className='mb-12'><span className='headerUnderline'>Our</span> way
                     <span className='text-mainRed'>.</span></h2>
+                <div className='flex items-center'>
+                    <h3 className='articleTitle text-mainRed pl-2'>Appliance repair company</h3>
 
-
-                <div className='flex'>
-                    <h3 className='articleTitle'>Appliance repair company</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur. Tincidunt vitae vel ut mauris libero quis morbi. Ut
-                        habitasse nisi mollis sapien. Vitae morbi faucibus cras nisi dolor erat turpis integer lorem.
-                        Dui interdum aliquet nam augue ultrices fermentum. Eget curabitur ante amet in sodales eu massa
-                        et. Id pellentesque tincidunt ac eu nunc pellentesque nunc.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur. Tincidunt vitae vel ut mauris libero quis morbi. Ut
-                        habitasse nisi mollis sapien. Vitae morbi faucibus cras nisi dolor erat turpis integer lorem.
-                        Dui interdum aliquet nam augue ultrices fermentum. Eget curabitur ante amet in sodales eu massa
-                        et. Id pellentesque tincidunt ac eu nunc pellentesque nunc.</p>
+                    <div className='flex justify-between text-xs sm:text-sm lg:text-base xl:text-xl text-white'>
+                        <p className='pr-1.5 md:pr-3 lg:pr-5 xl:pr-7'>Lorem ipsum dolor sit amet consectetur. Tincidunt
+                            vitae
+                            vel ut mauris libero quis morbi. Ut
+                            habitasse nisi mollis sapien. Vitae morbi faucibus cras nisi dolor erat turpis integer
+                            lorem. Dui interdum aliquet nam augue ultrices fermentum. Eget curabitur ante amet in
+                            sodales eu
+                            massa et. Id pellentesque tincidunt ac eu nunc pellentesque nunc.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur. Tincidunt vitae vel ut mauris libero quis morbi. Ut
+                            habitasse nisi mollis sapien. Vitae morbi faucibus cras nisi dolor erat turpis integer
+                            lorem. Dui interdum aliquet nam augue ultrices fermentum. Eget curabitur ante amet in
+                            sodales eu
+                            massa et. Id pellentesque tincidunt ac eu nunc pellentesque nunc.</p>
+                    </div>
                 </div>
-
             </section>
 
+            <h1 className='containerStyle pb-4 xl:pb-7'>Lorem ipsum dolor sit amet consectetur
+                <span className='text-mainRed'>.</span></h1>
+
+            <img src={MasterImg} alt="master_img" className='pb-9 sm:pb-11 lg:pb-20 xl:pb-[120px]'/>
         </main>
     );
 };
